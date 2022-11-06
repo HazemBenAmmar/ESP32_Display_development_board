5145613b437587965026c309ace595c4db439a19
hazem ben ammar <benammarhazem@lab619.tn>
2022-11-06 22:54:26 +0200

    added files for 2.8

---
+++ 2.4/2.4inch_SPI_Module_ILI9341/1-Demo/Demo_STM32/Demo_STM32F103RCT6_Software_SPI/HARDWARE/SPI/SPI.c
@@ -0,0 +1,64 @@
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//测试硬件：单片机STM32F103RCT6,正点原子MiniSTM32开发板,主频72MHZ，晶振12MHZ
+//TFT液晶驱动 for STM32 
+//淘宝网站：http://jczn.taobao.com
+//All rights reserved
+/****************************************************************************************************
+//=========================================电源接线================================================//
+//     LCD模块                STM32单片机
+//      VCC          接        DC5V/3.3V      //电源
+//      GND          接          GND          //电源地
+//=======================================液晶屏数据线接线==========================================//
+//本模块默认数据总线类型为SPI总线
+//     LCD模块                STM32单片机    
+//    SDI(MOSI)      接          PB15         //液晶屏SPI总线数据写信号
+//    SDO(MISO)      接          PB14         //液晶屏SPI总线数据读信号，如果不需要读，可以不接线
+//=======================================液晶屏控制线接线==========================================//
+//     LCD模块 					      STM32单片机 
+//       LED         接          PB9          //液晶屏背光控制信号，如果不需要控制，接5V或3.3V
+//       SCK         接          PB13         //液晶屏SPI总线时钟信号
+//      DC/RS        接          PB10         //液晶屏数据/命令控制信号
+//       RST         接          PB12         //液晶屏复位控制信号
+//       CS          接          PB11         //液晶屏片选控制信号
+//=========================================触摸屏触接线=========================================//
+//如果模块不带触摸功能或者带有触摸功能，但是不需要触摸功能，则不需要进行触摸屏接线
+//	   LCD模块                STM32单片机 
+//      T_IRQ        接          PC10         //触摸屏触摸中断信号
+//      T_DO         接          PC2          //触摸屏SPI总线读信号
+//      T_DIN        接          PC3          //触摸屏SPI总线写信号
+//      T_CS         接          PC13         //触摸屏片选控制信号
+//      T_CLK        接          PC0          //触摸屏SPI总线时钟信号
+**************************************************************************************************/	
+ /* @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, QD electronic SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+**************************************************************************************************/	
+#include "spi.h"
+
+/*****************************************************************************
+ * @name       :void  SPIv_WriteData(u8 Data)
+ * @date       :2018-08-09 
+ * @function   :Write a byte of data using STM32's Software SPI
+ * @parameters :Data:Data to be written
+ * @retvalue   :None
+******************************************************************************/
+void  SPIv_WriteData(u8 Data)
+{
+	unsigned char i=0;
+	for(i=8;i>0;i--)
+	{
+	  if(Data&0x80)	
+	  SPI_MOSI_SET; //输出数据
+      else SPI_MOSI_CLR;
+	   
+      SPI_SCLK_CLR;       
+      SPI_SCLK_SET;
+      Data<<=1; 
+	}
+}
